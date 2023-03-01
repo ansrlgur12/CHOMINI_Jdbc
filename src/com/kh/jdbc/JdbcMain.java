2078c4ba8b4ffb7ed94475af15a24e64536807a6
ansrlgur12 <ansrlgur12@naver.com>
2023-03-01 20:06:57 +0900

    3.1 메뉴, 사이즈 DAO, VO

--- src/com/kh/jdbc/JdbcMain.java
+++ src/com/kh/jdbc/JdbcMain.java
@@ -1,16 +1,27 @@
 package com.kh.jdbc;
 
 import com.kh.jdbc.dao.CusDao;
+import com.kh.jdbc.dao.MenuDao;
+import com.kh.jdbc.dao.SizeDao;
 import com.kh.jdbc.vo.CusVO;
+import com.kh.jdbc.vo.MenuVO;
+import com.kh.jdbc.vo.SizeVO;
 
 import java.util.List;
 
 public class JdbcMain {
     public static void main(String[] args) {
 
-        CusDao dao = new CusDao();
-        List<CusVO> list = dao.cusSelect();
-        dao.cusSelectPrint(list);
+       // CusDao dao = new CusDao();
+       // List<CusVO> list = dao.cusSelect();
+       // dao.cusSelectPrint(list);
 
+       // MenuDao dao = new MenuDao();
+       // List<MenuVO> list = dao.menuSelect();
+       // dao.menuSelectPrint(list);
+
+        SizeDao dao = new SizeDao();
+        List<SizeVO> list = dao.sizeSelect();
+        dao.sizeSelectPrint(list);
     }
 }
--- src/com/kh/jdbc/dao/SubDao.java
+++
@@ -1,6 +0,0 @@
-package com.kh.jdbc.dao;
-
-public class SubDao {
-
-    // 푸쉬푸쉬베베ff
-}
